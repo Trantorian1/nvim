d25ca34c4f59131339d99d1e0584cde390b51806
trantorian <114066155+Trantorian1@users.noreply.github.com>
2026-06-12 18:11:17 +0200

    feat(nvim): nix lsp

--- nvim/ftplugin/nix.lua
+++ nvim/ftplugin/nix.lua
@@ -3,3 +3,10 @@ vim.o.sts = 2
 vim.o.sw = 2
 
 vim.treesitter.start()
+
+-- Exit if the language server isn't available
+if vim.fn.executable("nil") ~= 1 then
+	return
+end
+
+vim.lsp.enable("nil_ls")
